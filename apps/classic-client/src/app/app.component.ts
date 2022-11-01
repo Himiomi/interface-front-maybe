d4c525dca19c20ab0bc8644a9929fd7e6497206d
Florian Morin <fmorin@et.esiea.fr>
2022-11-01 12:47:35 +0100

    Initial commit

---
+++ apps/classic-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'interface-front-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'classic-client';
+}
